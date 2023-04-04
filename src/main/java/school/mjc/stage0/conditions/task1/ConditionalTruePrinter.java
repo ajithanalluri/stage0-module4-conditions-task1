9983e5f6d3085a4aafaaa58314ea27aa6ae1c8f4
EPAM\Ajitha_Nalluri <Ajitha_Nalluri@epam.com>
2023-04-04 15:54:58 +0530

    #1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -1,6 +1,10 @@
 package school.mjc.stage0.conditions.task1;
 
+import java.util.Arrays;
+
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        if (true)
+            System.out.println(true);
     }
 }
